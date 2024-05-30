f7fafb05c767788becbaa05692060d5e0bc349c5
rutratabosa <32820090-rutratabosa@users.noreply.replit.com>
2024-05-30 18:55:58 +0000

    Minha atividade de hoje Grupo Madre Artur Tabosa
    Grupo Madre Artur Tabosa

---
+++ README.md
@@ -0,0 +1,4 @@
+## ENTREGA DA SEMANA QUE VEM
+
+1. Ampliar os dados salvos do usuário além de username e password, ex: CPF, Telefone, Email
+2. Adicionar um CRUD a mais dentro da aplicação, pode ser já um dos CRUDs do projeto da equipe
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { error } = require("console");
 const express = require("express");
 const sqlite = require("sqlite3").verbose();
 
@@ -6,11 +7,9 @@ const port = 3000;
 const db = new sqlite.Database("database.db");
 
 db.serialize(() => {
-	db.run(
-		"CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)",
-	);
-
-	// db.run("INSERT INTO users (username, password) VALUES ('admin', 'admin')")
+	db.run("CREATE TABLE IF NOT EXISTS Perfil (Id_Perfil INTEGER IDENTITY PRIMARY KEY, Nome_Perfil VARCHAR(30) NOT NULL, Descricao_Perfil VARCHAR(30) NOT NULL)");
+	
+	db.run("CREATE TABLE IF NOT EXISTS Usuario (CPF_Usuario CHAR(11) PRIMARY KEY NOT NULL, Id_Perfil INTEGER REFERENCES Perfil(Id_Perfil), Nome_Usuario VARCHAR(60) NOT NULL, RG_Usuario VARCHAR(20) NOT NULL, Orgao_Emissor VARCHAR (6) NOT NULL, Nascimento DATE NOT NULL, Telefone VARCHAR(20) NOT NULL, Email VARCHAR(50) NOT NULL UNIQUE, Sexo CHAR(1) NOT NULL)");
 });
 
 app.use(express.json());
@@ -19,8 +18,8 @@ app.get("/", (req, res) => {
 	res.send("Estou na minha API");
 });
 
-app.get("/users", (req, res) => {
-	db.all("SELECT * FROM users", (error, rows) => {
+app.get("/Perfil", (req, res) => {
+	db.all("SELECT * FROM Perfil", (error, rows) => {
 		if (error) {
 			res.send(error);
 		}
@@ -28,11 +27,8 @@ app.get("/users", (req, res) => {
 	});
 });
 
-// pegar por ID
-
-app.get("/users/:id", (req, res) => {
-	const id = req.params.id;
-	db.all("SELECT * FROM users WHERE id = ?", id, (error, rows) => {
+app.get("/Usuario", (req, res) => {
+	db.all("SELECT * FROM Usuario", (error, rows) => {
 		if (error) {
 			res.send(error);
 		}
@@ -40,34 +36,173 @@ app.get("/users/:id", (req, res) => {
 	});
 });
 
+app.get("/Perfil/:Id_Perfil", (req, res) => {
+	const Id = req.params.Id_Perfil;
+	db.all("SELECT * FROM Perfil WHERE Id_Perfil = ?", Id, (error, rows) => {
+		if (error) {
+			res.send(error);
+		}
+		res.send(rows);
+	});
+});
 
-app.post("/users", (req, res) => {
-	const { username, password } = req.body;
+app.get("/Usuario/:CPF", (req, res) => {
+	const CPF = req.params.CPF;
+	db.all("SELECT * FROM Usuario WHERE CPF_Usuario = ?", CPF, (error, rows) => {
+		if (error) {
+			res.send(error);
+		}
+		res.send(rows);
+	});
+});
+
+app.post("/Perfil", (req, res) => {
+	const { Id_Perfil, Nome_Perfil, Descricao_Perfil } = req.body;
 	console.log(req.body);
 
-	if (
-		username == "undefined" ||
-		username == "" ||
-		password == "undefined" ||
-		password == ""
-	) {
+	if ( !Id_Perfil || !Nome_Perfil || !Descricao_Perfil) {
 		res.send("Dados incompletos");
 		return;
 	} else {
 		db.run(
-			"INSERT INTO users (username, password) VALUES (?, ?)",
-			[username, password],
+			"INSERT INTO Perfil (Id_Perfil, Nome_Perfil, Descricao_Perfil) VALUES (?, ?, ?)",
+			[Id_Perfil, Nome_Perfil, Descricao_Perfil],
 			(error) => {
 				if (error) {
 					res.send(error);
 					return;
 				}
-				res.send(`Usuário ${username} cadastrado com sucesso`);
+				res.send(`Perfil ${Nome_Perfil} cadastrado com sucesso`);
 			},
 		);
 	}
 });
 
+app.post("/Usuario", (req, res) => {
+	const { CPF_Usuario, Id_Perfil, Nome_Usuario, RG_Usuario, Orgao_Emissor,Nascimento, Telefone, Email, Sexo } = req.body;
+
+	if (!CPF_Usuario || !Id_Perfil || !Nome_Usuario || !RG_Usuario || !Orgao_Emissor || !Nascimento || !Telefone || !Email || !Sexo) {
+		res.send("Dados incompletos");
+		return;
+	} else {
+		db.run("INSERT INTO Usuario (CPF_Usuario, Id_Perfil, Nome_Usuario, RG_Usuario, Orgao_Emissor,Nascimento, Telefone, Email, Sexo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+[CPF_Usuario, Id_Perfil, Nome_Usuario, RG_Usuario, Orgao_Emissor,Nascimento, Telefone, Email, Sexo], (error) => {
+				if (error) {
+					res.send(error);
+					return;
+				}
+				console.log(req.body)
+				res.send(`Usuário ${Nome_Usuario} cadastrado com sucesso`);
+			},
+		);
+	}
+});
+	
+app.put("/Perfil/:Id_Perfil", (req, res) => {
+	const Id = req.params.Id_Perfil;
+	const { Id_Perfil, Nome_Perfil, Descricao_Perfil } = req.body;
+	if (!Id_Perfil || !Nome_Perfil || !Descricao_Perfil){
+		res.send("Dados incompletos");
+		return;
+	}else{
+		db.run(
+			"UPDATE Perfil SET Nome_Perfil = ?, Descricao_Perfil = ? WHERE Id_Perfil = ?",
+			[Nome_Perfil, Descricao_Perfil, Id],
+			(error) => {
+				if (error) {
+					res.send(error);
+					return;
+				}
+				console.log(req.body)
+				res.send(`Usuário com a Id: ${Id_Perfil} atualizado com sucesso`);
+			},
+		);
+	}
+})
+
+app.put("/Usuario/:CPF", (req, res) => {
+	const CPF = req.params.CPF;
+	const { Nome_Usuario, RG_Usuario, Orgao_Emissor,Nascimento, Telefone, Email, Sexo } = req.body;
+	if (!Nome_Usuario || !RG_Usuario || !Orgao_Emissor || !Nascimento || !Telefone || !Email || !Sexo){
+		res.send("Dados incompletos");
+		return;
+	}else{
+		db.run("UPDATE Usuario SET Nome_Usuario = ?, RG_Usuario = ?, Orgao_Emissor = ?, Nascimento = ?, Telefone = ?, Email = ?, Sexo = ? WHERE CPF = ?",
+			[Nome_Usuario, RG_Usuario, Orgao_Emissor,Nascimento, Telefone, Email, Sexo, CPF],
+			(error) => {
+				if (error) {
+					res.send(error);
+					return;
+				}
+				res.send(`Usuário com o CPF: ${CPF} atualizado com sucesso`);
+			},
+		);
+	}
+})
+	
+app.patch("/Perfil/:Id_Perfil", (req, res) =>{
+	const Id = req.params.Id;
+	const { Id_Perfil, Nome_Perfil, Descricao_Perfil } = req.body;
+	if (!Id_Perfil || !Nome_Perfil || !Descricao_Perfil){
+		res.send("Dados incompletos");
+		return;
+	}else{
+		db.run(
+			"UPDATE Perfil SET Id_Perfil = ?, Nome_Perfil = ?, Descricao_Perfil = ? WHERE Id_Perfil = ?",
+			[Nome_Perfil, Descricao_Perfil, Id],
+			(error) => {
+				if (error) {
+					res.send(error);
+					return;
+				}
+				res.send(`Usuário com o Id: ${Id_Perfil} atualizado com sucesso`);
+			},
+		);
+	}
+})
+
+app.patch("/Usuario/:CPF", (req, res) =>{
+	const CPF = req.params.CPF;
+	const { Nome_Usuario, RG_Usuario, Orgao_Emissor,Nascimento, Telefone, Email, Sexo } = req.body;
+	if (!Nome_Usuario || !RG_Usuario || !Orgao_Emissor || !Nascimento || !Telefone || !Email || !Sexo){
+		res.send("Dados incompletos");
+		return;
+	}else{
+		db.run("UPDATE Usuario SET Nome_Usuario = ?, RG_Usuario = ?, Orgao_Emissor = ?, Nascimento = ?, Telefone = ?, Email = ?, Sexo = ? WHERE CPF = ?",
+			[Nome_Usuario, RG_Usuario, Orgao_Emissor,Nascimento, Telefone, Email, Sexo, CPF],
+			(error) => {
+				if (error) {
+					res.send(error);
+					return;
+				}
+				res.send(`Usuário com o CPF: ${CPF} atualizado com sucesso`);
+			},
+		);
+	}
+})
+	
+app.delete("/Perfil/:Id_Perfil", (req, res) =>{
+	const id = req.params.id;
+	db.run("DELETE FROM Perfil WHERE Id_Perfil = ?", id, (error) => {
+		if (error) {
+			res.send(error);
+			return;
+		}
+		res.send(`Usuário ${id} deletado com sucesso`);
+	});
+})
+
+app.delete("/Usuario/:CPF", (req, res) =>{
+	const CPF = req.params.CPF;
+	db.run("DELETE FROM Usuario WHERE CPF = ?", id, (error) => {
+		if (error) {
+			res.send(error);
+			return;
+		}
+		res.send(`Usuário ${CPF} deletado com sucesso`);
+	});
+})
+	
 app.listen(port, () => {
 	console.log(`Servidor rodando na porta ${port}`);
 });
